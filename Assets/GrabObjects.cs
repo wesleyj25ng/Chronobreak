1b0855417bf3b6aeee719da45c06ecb0441c7360
wesleyj25ng <wesleyj25ng@gmail.com>
2023-11-11 19:09:46 -0500

    Grab effect adjustments
    allows items to be grabbed while time is frozen

--- Assets/GrabObjects.cs
+++ Assets/GrabObjects.cs
@@ -8,26 +8,31 @@ public class GrabObjects : MonoBehaviour
     public Transform boxHolder;
     public float maxRayDist = 1.5f; // Maximum ray distance
     private int layerIndex;
+    private Collider2D playerCollider; // Reference to the player's collider
+    private TimeManager timemanager;
 
     private void Start()
     {
         layerIndex = LayerMask.NameToLayer("Object");
+        playerCollider = GetComponent<Collider2D>();
+        timemanager = GameObject.FindGameObjectWithTag("TimeManager").GetComponent<TimeManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit2D grabCheck = Physics2D.Raycast(grabDetect.position, Vector2.right * transform.localScale, maxRayDist, 1 << layerIndex);
+        RaycastHit2D grabCheck = Physics2D.Raycast(grabDetect.position, Vector2.right * transform.localScale.x, maxRayDist, 1 << layerIndex);
 
         if (grabCheck.collider != null && grabCheck.collider.gameObject.layer == layerIndex)
         {
-            float currentRayDist = Mathf.Clamp(Vector2.Distance(transform.position, grabCheck.point), 0, maxRayDist);
-
             if (Input.GetKeyDown(KeyCode.G))
             {
                 Rigidbody2D rb2d = grabCheck.collider.gameObject.GetComponent<Rigidbody2D>();
                 if (rb2d != null)
                 {
+                    // Disable collision between the player and the grabbed object
+                    Physics2D.IgnoreCollision(playerCollider, grabCheck.collider, true);
+
                     grabCheck.collider.gameObject.transform.parent = boxHolder;
                     rb2d.velocity = Vector2.zero; // Stop any existing movement
                     rb2d.isKinematic = true;
@@ -42,9 +47,18 @@ public class GrabObjects : MonoBehaviour
                 Rigidbody2D rb2d = grabCheck.collider.gameObject.GetComponent<Rigidbody2D>();
                 if (rb2d != null)
                 {
+                    // Re-enable collision between the player and the grabbed object
+                    Physics2D.IgnoreCollision(playerCollider, grabCheck.collider, false);
+
                     grabCheck.collider.gameObject.transform.parent = null;
                     rb2d.isKinematic = false;
-                }
+
+                    if (timemanager.TimeIsStopped)
+                    {
+                        rb2d.velocity = Vector2.zero; //makes the rigidbody stop moving
+                        rb2d.isKinematic = true; //not affected by forces
+                    }
+                } 
             }
         }
     }
